56eddd7faaba5b1a90ac2858ab24b59544725176
hesamzahedmousavi <hesambsusb0@gmail.com>
2025-03-06 16:13:45 +0330

    Fixed Commit

--- src/components/DisplayWarning/DisplayWarning.jsx
+++ src/components/DisplayWarning/DisplayWarning.jsx
@@ -1,31 +1,24 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 const ResolutionBlock = () => {
-  const [isBlocked, setIsBlocked] = useState(false);
-
   useEffect(() => {
     const checkResolution = () => {
       if (window.innerWidth < 1280) {
         alert(
           "این سایت هنوز برای گوشی‌های موبایل بهینه‌سازی نشده است! لطفاً از دستگاه دسکتاپ استفاده کنید."
         );
-        setIsBlocked(true);
+        document.body.style.display = "none";
       }
     };
 
     checkResolution();
     window.addEventListener("resize", checkResolution);
 
-    return () => window.removeEventListener("resize", checkResolution);
+    return () => {
+      document.body.style.display = "block";
+      window.removeEventListener("resize", checkResolution);
+    };
   }, []);
-
-  if (isBlocked) {
-    return (
-      <div className="fixed inset-0 bg-black text-white flex items-center justify-center text-xl font-bold">
-        دسترسی به سایت در حالت موبایل محدود شده است.
-      </div>
-    );
-  }
 
   return null;
 };
